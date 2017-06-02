9b04227b41e18c3aabeeadbfebb2b8b9bfd11447
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2017-06-02 14:50:10 +0300

    debugging the grid/planet borders, rover now wraps around grid

--- rover.js
+++ rover.js
@@ -1,25 +1,102 @@
+//the rover object
 var myRover = {
-  position: [0,0],
-  direction: 'N'
+  position : [0, 0],
+  direction : "N"
 };
 
-function goForward(rover) {
-  switch(rover.direction) {
-    case 'N':
-      rover.position[0]++
-      break;
-    case 'E':
-      rover.position[1]++
-      break;
-    case 'S':
-      rover.position[0]--
-      break;
-    case 'W':
-      rover.position[1]--
-      break;
-  };
 
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+
+//command to move forward
+var f = function(rover){
+  switch(rover.direction){
+    case "N" : rover.position[1]++;
+    break;
+    case "S" : rover.position[1]--;
+    break;
+    case "E" : rover.position[0]++;
+    break;
+    case "W" : rover.position[0]--;
+    break;
+    default  :
+    console.log("I don't recognise this direction");
+  }
+  /*console.log("The vehicle has moved into position :[" + rover.position[0] + ", " + rover.position[1] + "] ");*/
+
+};
+
+//command to move backwards
+var b = function(rover){
+  switch(rover.direction){
+    case "N" : rover.position[1]--;
+    break;
+    case "S" : rover.position[1]++;
+    break;
+    case "E" : rover.position[0]--;
+    break;
+    case "W" : rover.position[0]++;
+    break;
+    default  :
+    console.log("I don't recognise this direction");
+  }
+  /*console.log("The vehicle has moved into position :[" + rover.position[0] + ", " + rover.position[1] + "] ");*/
+
+};
+
+//command to face left
+var l = function(rover){
+    rover.direction = "W";
+    console.log("The vehicle is facing at a new direction : " + rover.direction);
+
+};
+
+//command to face right
+var r = function(rover){
+    rover.direction = "E";
+    console.log("The vehicle is facing at a new direction : " + rover.direction);
+
+};
+
+//prompting the user for instructions
+var ins = prompt();
+
+//looping through the users sequence of instruction letters
+for (i = 0; i < ins.length; i++){
+
+//checking for applicable commands and exectuing accordingly
+  if(ins[i] === "f"){
+    f(myRover);
+  }else if(ins[i] === "b"){
+    b(myRover);
+  }else if(ins[i] === "l"){
+    l(myRover);
+  }else if(ins[i] === "r"){
+    r(myRover);
+  }
+
+  //wrapping around the planet grid when reaching the borders
+    if(myRover.position[1] < 0){
+      myRover.position[1] = 10;
+    }
+    if(myRover.position[1] > 10){
+      myRover.position[1] = 0;
+    }
+    if(myRover.position[0] < 0){
+      myRover.position[0] = 10;
+    }
+    if(myRover.position[0] > 10){
+      myRover.position[0] = 0;
+    }
+
+//breaking and surfacing error to user whilst logging rover's last position
+  if(ins[i] !== "f" && ins[i] !== "b" && ins[i] !== "l" && ins[i] !== "r" ){
+    console.log("Error 1!!!!!! Rover could move any further, command number " + [i + 1] + ", " + "\"" + ins[i] + "\"" +  " is not recognised");
+    console.log("\n");
+    console.log("Rover has stopped at position: " + myRover.position);
+    console.log("Please review your array of instructions: " + ins);
+    break;
+
+//else success message and logging of final position
+  }else{
+  console.log("The vehicle has successfully reached position: " + myRover.position + " following your commands");
+  }
 }
-
-goForward(myRover);
